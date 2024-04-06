b4bc502150c553deeca91fe25800b30ebdc8edec
pbokolo <dev.pbokolo@gmail.com>
2024-04-06 18:59:07 +0200

    Creating the sendRequest function that connect to the api and gets the response

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 "use strict";
 
+const WEATHERAPI_KEY = "68c1d6e4972945f1a20164852240604";
+const WEATHERAPI_ADDRESS = `http://api.weatherapi.com/v1/current.json?key=${WEATHERAPI_KEY}&q=London`;
+
+async function sendRequest() {
+  try {
+    const response = await fetch(WEATHERAPI_ADDRESS);
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 function init() {
-  console.log("Launching");
+  sendRequest();
 }
 
 init();
