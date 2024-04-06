143cbd9c5e7e17e9f98168c2310ae7e2f22897a7
pbokolo <dev.pbokolo@gmail.com>
2024-04-06 20:48:05 +0200

    Creating the render function to render api data into the html

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const API_KEY = "68c1d6e4972945f1a20164852240604";
 const API = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
 
+const placeLbl = document.querySelector("#place");
+const conditionIcon = document.querySelectorAll("#conditionIcon");
+
 function getGeoCoordinates() {
   navigator.geolocation.getCurrentPosition(
     (position) => {
@@ -13,11 +16,17 @@ function getGeoCoordinates() {
   );
 }
 
+function render(data) {
+  placeLbl.textContent = data.location.name;
+  conditionIcon.src = data.current.condition.icon;
+}
+
 async function sendRequest(lat, lng) {
   try {
     const response = await fetch(`${API}&q=${lat},${lng}`);
     const result = await response.json();
     console.log(result);
+    render(result);
   } catch (error) {
     console.log(error);
   }
