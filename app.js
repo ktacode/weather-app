8996381c8e2637bfd3f800f89ed2c3bd0ef29dcc
pbokolo <dev.pbokolo@gmail.com>
2024-04-06 19:04:50 +0200

    Creating the getGeoCoordinates function to get the latitude and longitude of the user so that they can be sent to the api

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 "use strict";
 
-const WEATHERAPI_KEY = "68c1d6e4972945f1a20164852240604";
-const WEATHERAPI_ADDRESS = `http://api.weatherapi.com/v1/current.json?key=${WEATHERAPI_KEY}&q=London`;
+const API_KEY = "68c1d6e4972945f1a20164852240604";
+const API = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=London`;
+
+function getGeoCoordinates() {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      console.log(latitude, longitude);
+    },
+    (error) => console.log(error)
+  );
+}
 
 async function sendRequest() {
   try {
-    const response = await fetch(WEATHERAPI_ADDRESS);
+    const response = await fetch(API);
     const result = await response.json();
     console.log(result);
   } catch (error) {
@@ -14,7 +24,8 @@ async function sendRequest() {
 }
 
 function init() {
-  sendRequest();
+  getGeoCoordinates();
+  //   sendRequest();
 }
 
 init();
