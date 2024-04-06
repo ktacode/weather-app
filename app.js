0d3498e5b19237a6ad6cb5f9c4b3df1c1ee0a76f
pbokolo <dev.pbokolo@gmail.com>
2024-04-06 19:07:50 +0200

    Modifiying the code of the sendRequest fonction to get geo location data from outside

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 "use strict";
 
 const API_KEY = "68c1d6e4972945f1a20164852240604";
-const API = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=London`;
+const API = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
 
 function getGeoCoordinates() {
   navigator.geolocation.getCurrentPosition(
     (position) => {
       const { latitude, longitude } = position.coords;
-      console.log(latitude, longitude);
+      sendRequest(latitude, longitude);
     },
     (error) => console.log(error)
   );
 }
 
-async function sendRequest() {
+async function sendRequest(lat, lng) {
   try {
-    const response = await fetch(API);
+    const response = await fetch(`${API}&q=${lat},${lng}`);
     const result = await response.json();
     console.log(result);
   } catch (error) {
@@ -25,7 +25,6 @@ async function sendRequest() {
 
 function init() {
   getGeoCoordinates();
-  //   sendRequest();
 }
 
 init();
